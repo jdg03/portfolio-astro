dcadf5a26fb48da2dc41ffef0522bc3997245ef3
José David Gonzalez <gonzalez5.jose52@gmail.com>
2025-11-27 20:19:09 -0600

    add experience section

---
+++ src/constants/Experience.js
@@ -0,0 +1,12 @@
+const Experiences = [
+  {
+    title: "Practicante de Desarrollo de Software",
+    company: "BANHCAFE",
+    date: "Febrero 2025 - Julio 2025",
+    description:
+      "Levantamiento y análisis de requerimientos funcionales con usuarios internos. Desarrollo de microservicios con ASP NET.Creación y optimización de procedimientos de almacenado con SQL Server. Generaciónde reportes con SQL server y Report Builder. Mantenimiento de módulos web internos del banco.",
+    link: "#",
+  },
+];
+
+export default Experiences;
