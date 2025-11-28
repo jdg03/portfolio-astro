a8d325df9410489dbe680f6ddc6a7b3326fa00d9
José David Gonzalez <gonzalez5.jose52@gmail.com>
2025-11-28 04:44:31 -0600

    add certificates sections

--- src/constants/Experience.js
+++ src/constants/Experience.js
@@ -3,8 +3,13 @@ const Experiences = [
     title: "Practicante de Desarrollo de Software",
     company: "BANHCAFE",
     date: "Febrero 2025 - Julio 2025",
-    description:
-      "Levantamiento y análisis de requerimientos funcionales con usuarios internos. Desarrollo de microservicios con ASP NET.Creación y optimización de procedimientos de almacenado con SQL Server. Generaciónde reportes con SQL server y Report Builder. Mantenimiento de módulos web internos del banco.",
+    description: [
+      "Levantamiento y análisis de requerimientos funcionales con usuarios internos.",
+      "Desarrollo de microservicios con ASP.NET.",
+      "Creación y optimización de procedimientos almacenados con SQL Server.",
+      "Generación de reportes con SQL Server y Report Builder.",
+      "Mantenimiento de módulos web internos del banco.",
+    ],
     link: "#",
   },
 ];
